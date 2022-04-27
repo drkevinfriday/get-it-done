d9ad84370611bce3e6612dd11933394d6533dec5
Kevin Friday <kevinfriday87@gmail.com>
2022-04-26 19:59:13 -0500

    fetch request udate

--- assets/js/hompage.js
+++ assets/js/hompage.js
@@ -1,7 +1,25 @@
-var  getUserRepos = function () {
-    fetch('https://api.github.com/users/octocat/repos');
-    console.log("fucntion was called");
+
+
+
+
+
+var  getUserRepos = function (user) {
+
+    var targetApi = "https://api.github.com/users/" + user + "/repos";
+
+    console.log(targetApi)
+
+    //fetches the response from the repo
+    fetch(targetApi).then(function(response){
+        //takes the resonse and turns into a json data object
+        response.json().then(function(data){
+            //displays the data in the console log 
+            console.log(data)
+        })
+    });
+    //reads the fetch resonse 
 
 };
 
-getUserRepos();
+
+getUserRepos("drkevinfriday");
